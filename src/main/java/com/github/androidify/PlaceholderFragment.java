1189b3647006d18aac54b3fc91a5f332bf7e35a8
LArchaon <alex.buzdin@gmail.com>
2014-04-10 22:36:22 +0300

    added viewPagers to fragment

---
+++ src/main/java/com/github/androidify/PlaceholderFragment.java
@@ -0,0 +1,17 @@
+package com.github.androidify;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PlaceholderFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
