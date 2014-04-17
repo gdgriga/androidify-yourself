d88a5c9a93896d02ea34f8b79c338fab921c5cf3
echoAlexey <aleksejs.nikolajenko@gmail.com>
2014-04-17 19:29:47 +0300

    added hello world layout as 1st step of workshop

--- src/main/java/com/github/androidify/MainActivity.java
+++ src/main/java/com/github/androidify/MainActivity.java
@@ -1,6 +1,13 @@
 package com.github.androidify;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.helloworld);
+    }
 }
